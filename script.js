3f65d79605335f9ec11cf6e79d8f95fee67fb144
Harshvardhan Mestha <harshvardhanmestha@yahoo.com>
2023-01-07 20:53:29 +0530

    This commit fixes bugs on Safari.

--- script.js
+++ script.js
@@ -5,16 +5,17 @@
 
 for (var i = 0;i<256;i++) {
     var container = document.getElementById("grid");
-    container.innerHTML += '<div class="tile_no_hover" id="tile"></div>';
+    container.innerHTML += '<div class="no_hover" id="tile"></div>';
 }
 
 const tiles = document.querySelectorAll('div')
 tiles.forEach(tile => tile.addEventListener('mouseenter',toggleHover));
+tiles.forEach(tile => tile.addEventListener('mousedown',toggleHover));
 
 
 function toggleHover(e){
-    e.target.classList.remove('tile_no_hover')
-    e.target.classList.add('tile_hover')
+    e.target.classList.remove('no_hover');
+    e.target.classList.add('hover');
 }
 
 
